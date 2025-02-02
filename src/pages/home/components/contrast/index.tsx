f7a0c9ed90f439c192a34ddb383cf171db60d1aa
Azizbek <begmatovaziz444@gmail.com>
2025-02-03 00:42:28 +0500

    bug fixed

--- src/pages/home/components/contrast/index.tsx
+++ src/pages/home/components/contrast/index.tsx
@@ -1,9 +1,9 @@
 import { useTranslation } from 'react-i18next';
+import images from 'src/constants/images';
 import styles from './contrastAgents.module.scss';
 import img1 from '/src/assets/img/contrast1.png';
 import img2 from '/src/assets/img/contrast2.png';
 import img3 from '/src/assets/img/contrast3.png';
-import images from 'src/constants/images';
 
 const HomeContrastAgents = () => {
   const { t } = useTranslation();
@@ -26,8 +26,8 @@ const HomeContrastAgents = () => {
       <h2 className={styles.title}>{t('contrast.title1')}</h2>
       <p className={styles.text}>{t('contrast.desc')}</p>
       <div className={styles.imgContainer}>
-        <img src={images.contrast_img1} alt="" width={618} />
-        <img src={images.contrast_img2} alt="" width={618} />
+        <img src={images.contrast_img1} alt="img" width={618} />
+        <img src={images.contrast_img2} alt="img" width={618} />
       </div>
       <div className={styles.featureCards}>
         {featureCards.map((item, index) => (
